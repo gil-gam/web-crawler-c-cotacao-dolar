5165077afea9d3fe2ddfef492bfab262c721ef81
Gil Gam <gilbertoandreattamaia@gmail.com>
2021-11-04 22:58:04 -0300

    Adicionar arquivos de projeto.

---
+++ cotacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace cotacao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var html = @"https://br.investing.com/currencies/usd-brl";
+
+                HtmlWeb web = new HtmlWeb();
+
+                var htmlDoc = web.Load(html);
+
+                var caminho = "/html/body/div[5]/section/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/span[1]";
+
+                var node = htmlDoc.DocumentNode.SelectSingleNode(caminho);
+
+                Console.WriteLine(node.InnerText);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
